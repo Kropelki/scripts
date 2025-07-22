1da01e9ab4bda2bf365618eb2d05529d09a25c24
Paulina Kalicka <71526180+paulinek13@users.noreply.github.com>
2025-07-22 09:54:57 +0200

    update: make the script work regardless of where it's called from

--- influxdb/download-all-data.sh
+++ influxdb/download-all-data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
 if [ -f .env ]; then
   INFLUXDB_TOKEN=$(grep "^INFLUXDB_TOKEN=" .env | cut -d '=' -f2-)
 else
